4e37a35c6a666f4e65fda61b7fc8a2b35fb0304a
Christian <christian.fernandez@nyu.edu>
2017-12-03 23:24:21 +0000

    Updated basic site unit test

--- src/tests/test_sites.py
+++ src/tests/test_sites.py
@@ -3,7 +3,16 @@
 import unittest
 
 from src.objects.site import Site
-
+from src.sites.data_manager import DataManager
+from src.sites.lock_manager import LockManager
 class SiteTestCase(unittest.TestCase):
     """ Test cases for a single Site """
 
+    def test_site_value(self):
+        """ Test that returns it's Id  """
+
+        dm = DataManager()
+        lm = LockManager()
+        site = Site(1, dm, lm)
+
+        self.assertTrue("1" in str(site))
